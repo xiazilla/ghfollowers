b57ad0d88c427ecbfdefd152fea624e8d4065635
Jamie Xia <jamie88xia@utexas.edu>
2020-06-18 22:56:30 -0500

    Initial Commit

---
+++ ghfollowers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ghfollowers
+//
+//  Created by Jamie Xia on 6/18/20.
+//  Copyright © 2020 cs378. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
