4518d3cce944787fe29bc8ef03bb9a2fbfb7778f
Jamie Xia <jamie88xia@utexas.edu>
2020-06-22 21:00:38 -0500

    initial commit

--- ghfollowers/SceneDelegate.swift
+++ ghfollowers/SceneDelegate.swift
@@ -17,9 +17,39 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene  = (scene as? UIWindowScene) else { return }
+        
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = createTabBar()
+        window?.makeKeyAndVisible()
+        
     }
-
+    
+    func createTabBar() -> UITabBarController {
+        let tabbar = UITabBarController();
+        UITabBar.appearance().tintColor = .systemGreen
+        tabbar.viewControllers = [createSearchNC(), createFavoritesListNC ()]
+        return tabbar
+    }
+    
+    func createSearchNC() -> UINavigationController {
+        let searchVC = SearchVC();
+        searchVC.title = "Search"
+        searchVC.tabBarItem = UITabBarItem(tabBarSystemItem: .search, tag: 0)
+        
+        return UINavigationController(rootViewController: searchVC)
+    }
+    
+    func createFavoritesListNC() -> UINavigationController {
+        let favoritesListVC = FavoritesListsVC();
+        favoritesListVC.title = "Favorites List"
+        favoritesListVC.tabBarItem = UITabBarItem(tabBarSystemItem: .favorites, tag: 1)
+        
+        return UINavigationController(rootViewController: favoritesListVC)
+        
+    }
+ 
     func sceneDidDisconnect(_ scene: UIScene) {
         // Called as the scene is being released by the system.
         // This occurs shortly after the scene enters the background, or when its session is discarded.
